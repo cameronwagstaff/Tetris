14fb2da03d9113174dab2bb8fb1c54c1c2f81af8
Matt Arnold <mattnold@yahoo.com>
2014-04-10 13:25:06 -0500

    Update Button.h

--- Button.h
+++ Button.h
@@ -1,42 +1,27 @@
-<<<<<<< HEAD
 /*******************************************************************************
-=======
-/*
->>>>>>> FETCH_HEAD
  * Author: Matt Arnold
  * Assignment Title:
  * Assignment Description:
  * Due Date:
  * Date Created: 01 April 2014
  * Date Last Modified: 02 April 2014 - Matt Arnold
-<<<<<<< HEAD
  ******************************************************************************/
-=======
- */
->>>>>>> FETCH_HEAD
-
 
 #ifndef Tetris_Button_h
 #define Tetris_Button_h
 
 #include <string>
-<<<<<<< HEAD
 #include <ctime>
-=======
->>>>>>> FETCH_HEAD
 #include "GLUT_Plotter.h"
 #include "Point.h"
 #include "Constants.h"
 
 using namespace std;
 
-<<<<<<< HEAD
 const int BUTTON_LENGTH = 310;
 const int BUTTON_WIDTH = 50;
-=======
 const int BUTTON_LENGTH = 160;
 const int BUTTON_WIDTH = 80;
->>>>>>> FETCH_HEAD
 
 class Button
 {
@@ -45,12 +30,9 @@ class Button
         Point topLeft, bottomRight;
         string text;
     public:
-<<<<<<< HEAD
         Button(string s = "TEXT", Point tl = Point(0,0),
                Point br = Point(BUTTON_LENGTH, BUTTON_WIDTH),
-=======
         Button(string s = "TEXT", Point tl = Point(0,0), Point br = Point(0, 0),
->>>>>>> FETCH_HEAD
                int color = WHITE,int textColor = BLACK);
         void draw(GLUT_Plotter* g);
         void erase(GLUT_Plotter* g);
@@ -58,7 +40,6 @@ class Button
         void setColor(unsigned int newColor);
         void setTextColor(unsigned int newTextColor);
         void setText(string newText);
-<<<<<<< HEAD
         void setTopLeft(Point a);
         void setBottomRight(Point b);
         unsigned int getColor();
@@ -67,12 +48,9 @@ class Button
         Point getTopLeft();
         Point getBottomRight();
         bool isInRange(Point a);
-    
-=======
         unsigned int getColor();
         unsigned int getTextColor();
         string getText();
->>>>>>> FETCH_HEAD
 };
 
 #endif
