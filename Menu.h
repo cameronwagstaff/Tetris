e42b2f0b05cecf766d2af2ad536fa193645fbe18
Matt Arnold <mattnold@yahoo.com>
2014-04-10 13:26:17 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -4,11 +4,8 @@
  * Assignment Description:
  * Due Date:
  * Date Created: 01 April 2014
-<<<<<<< HEAD
  * Date Last Modified: 08 April 2014 - Matt Arnold
-=======
  * Date Last Modified: 02 April 2014 - Matt Arnold
->>>>>>> FETCH_HEAD
  */
 
 
@@ -23,23 +20,16 @@ class Menu
 {
 private:
     Button startButton, viewScores;
-<<<<<<< HEAD
     bool running;
-    
-=======
 
->>>>>>> FETCH_HEAD
 public:
     Menu();
     void draw(GLUT_Plotter *g);
     void erase(GLUT_Plotter *g);
-<<<<<<< HEAD
     void run(GLUT_Plotter *g);
     Button getStartButton();
     Button getViewScores();
     bool operator !();
-=======
->>>>>>> FETCH_HEAD
 };
 
 #endif
