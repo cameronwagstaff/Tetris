cbd23524bbc4fb6716270222d75d54204ed8251d
johnhdaniel <johnh.daniel94@gmail.com>
2014-04-16 23:15:30 -0500

    Update Matrix.h
    More to come

--- Matrix.h
+++ Matrix.h
@@ -1,5 +1,21 @@
-#ifndef MATRIX_H_
-#define MATRIX_H_
+#ifndef TETRIS_MATRIX_H_
+#define TETRIS_MATRIX_H_
 
+#include "Constants.h"
+#include "Shape.h"
+
+class Matrix
+{
+    private:
+        Shape** matrix;
+
+    public:
+        Matrix();
+        //Matrix(const Matrix& other);
+        ~Matrix();
+        bool lineCheck();
+        void shiftDown();
+        Matrix& addShape(int row, int col);
+};
 
 #endif
