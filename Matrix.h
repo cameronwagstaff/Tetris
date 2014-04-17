807de67b0a18525cf9ec1331f481705522bc821d
johnhdaniel <johnh.daniel94@gmail.com>
2014-04-17 00:17:33 -0500

    Update Matrix.h
    This is the UpToDate Matrix Class
    It is great.
    I'm getting weird errors that say Multiple Definitions of every function. Try it on your machines and see if it's just because of my screwy git hub/gui/bash/whoknows

--- Matrix.h
+++ Matrix.h
@@ -1,3 +1,9 @@
+/******************************************************************************
+ * Author: John Daniel                                                        *
+ * Description: Matrix Class, all you need in a Matrix 2.0! (2-D)             *
+ * Date Created:16 April 2014                                                 *
+ * Date Last Modified:16 April 2014 - John Daniel                             *
+ *****************************************************************************/
 #ifndef TETRIS_MATRIX_H_
 #define TETRIS_MATRIX_H_
 
@@ -7,15 +13,22 @@
 class Matrix
 {
     private:
-        Shape** matrix;
+        /**
+         Yes three pointers. To determine whether a shape is there the
+         pointer either points to NULL or a shape. So it's a group of
+         pointers that point to pointers that point to NULL or SHAPES!
+        **/
+        Shape*** matrix;
 
     public:
         Matrix();
         //Matrix(const Matrix& other);
         ~Matrix();
-        bool lineCheck();
-        void shiftDown();
-        Matrix& addShape(int row, int col);
+        void lineCheck();
+        void shiftDown(int r);
+        int  rowSum(int r);
+        void deleteRow(int r);
+        Matrix& addShape(int row, int col, Shape& object);
 };
 
 #endif
