cbe615f6f3cd4dc70e3f3b1e5cccf81d7fc81b16
James Saul <antiochc4c@gmial.com>
2014-04-19 11:43:47 -0500

    Sorry last one for a little while
    Default constructor updated to use set color function.

--- Piece.cpp
+++ Piece.cpp
@@ -17,8 +17,9 @@ Return value:  None
 ******************************************************************************/
 Piece::Piece()
 {
-    color;
+
     pieceType = I;
+    setColor();
     Point p1, p2, p3, p4;
     rest = false;
 }
